92d1767a63701e550dd3fdcdb65bdf73c13cbf6a
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2024-08-10 14:32:50 +0200

    Update CartSummary.jsx

--- shopping-cart/src/components/CartSummary.jsx
+++ shopping-cart/src/components/CartSummary.jsx
@@ -6,7 +6,6 @@ export const CartSummary = () => {
   // Destructuring values from the ShopContext.
     const { cartItems } = useContext(ShopContext);
 
-  // Calculating the total number of items in the cart.
     const totalItems = Object.values(cartItems).reduce((total, quantity) => total + quantity, 0);
 
 return (
